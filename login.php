67b9fe913b7445c13a18c43b79f1078c547949d3
Hai072003 <96900664+Hai072003@users.noreply.github.com>
2024-05-10 17:24:09 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,12 +1,10 @@
 <?php
-// Thông tin kết nối đến cơ sở dữ liệu trên Aiven
 $db_host = "mysql-33be21d0-hainguuen03-bd23.l.aivencloud.com";
 $db_port = "15504";
 $db_name = "QUANLYSACH";
 $db_username = "avnadmin";
 $db_password = "AVNS_PX5a0PwZD0vjlbQA88-";
 
-// Kết nối đến cơ sở dữ liệu
 $conn = new mysqli($db_host, $db_username, $db_password, $db_name, $db_port);
 
 // Kiểm tra kết nối
@@ -14,7 +12,6 @@ if ($conn->connect_error) {
     die("Kết nối đến cơ sở dữ liệu thất bại: " . $conn->connect_error);
 }
 
-// Lấy dữ liệu từ form đăng nhập
 // Lấy dữ liệu từ form đăng nhập
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $TenUser = $_POST['TenUser'];
@@ -31,11 +28,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         header("Location: Sach.php");
         exit();
     } else {
-        // Đăng nhập thất bại
         echo ("Tên đăng nhập hoặc mật khẩu không chính xác!");
     }
 }
 
-// Đóng kết nối
 $conn->close();
 ?>
